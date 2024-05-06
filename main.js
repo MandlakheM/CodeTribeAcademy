64a00bc9b2358682b12153fa2850240201742b61
MandlakheM <mangumtamandilakhe7@gmail.com>
2024-05-06 11:22:56 +0200

    about me page fixed

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const name = "Mandilakhe";
+const firstName = "Mandilakhe";
 const surname = "Mangumta";
 let bio =
   "I am a freshly out of school junior developer is knowledge in programming languages such as Java, PHP, Visual studio basics.  As of last year 2023 I was doing my last year in Diploma in Information Technology at the Vaal University of Technology.  I hold various certifications from tech companies such as Amazon, Google, Cisco and Microsoft";
@@ -12,7 +12,17 @@ let projects = [
 const educationalBackground =
   "Diploma in Information Technology, Vaal University of Technology";
 
-console.log("Name:", name);
+document.getElementById("aboutMetext").textContent = firstName;
+document.getElementById("aboutMetext1").textContent = surname;
+document.getElementById("aboutMetext2").textContent = bio;
+//document.getElementById("aboutMetext3").textContent = project.title;
+projects.forEach((project, index) => {
+  document.getElementById("aboutMetext5").textContent =("Title:", project.title);
+  document.getElementById("aboutMetext6").textContent =("Description:", project.description);
+});
+document.getElementById("aboutMetext3").textContent = educationalBackground;
+
+console.log("Name:", fullName);
 console.log("Surname:", surname);
 console.log("Bio:", bio);
 console.log("Projects:");
@@ -22,3 +32,4 @@ projects.forEach((project, index) => {
   console.log("Description:", project.description);
 });
 console.log("Educational Background:", educationalBackground);
+display.textContent = { $surname };
